7e2d7ba18d1329280cf6dd63a3fc924d10676b53
VQTran123 <34687515+VQTran123@users.noreply.github.com>
2023-03-30 16:26:56 -0400

    added .sh file

---
+++ hw4/slurmSpectrum.sh
@@ -0,0 +1,5 @@
+module load spectrum-mpi cuda/11.2
+############################################################################################
+# Launch N tasks per compute node allocated. Per below this launches 6 MPI rank per compute# taskset insures that hyperthreaded cores are skipped.
+############################################################################################
+taskset -c 0-159:4 mpirun -N 6 /gpfs/u/home/SPNR/SPNRcaro/scratch/MPI-CUDA-Reduction/reduce
